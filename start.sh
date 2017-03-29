678be433877946bc29e454da3a5efe6ba544158a
Soichi Hayashi <soichih@gmail.com>
2017-03-29 14:29:59 +0000

    minor update to start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 
 pm2 delete sca-event
-pm2 start api/event.js --name sca-event --watch --ignore-watch="*.log test *.sh ui bin example"
+pm2 start api/event.js --name sca-event --watch --ignore-watch="*.log test *.sh ui bin example docker"
 
 pm2 delete sca-event-handler
-pm2 start bin/handle.js --name sca-event-handler --watch --ignore-watch="*.log test *.sh ui example"
+pm2 start bin/handle.js --name sca-event-handler --watch --ignore-watch="*.log test *.sh ui example docker"
 
 pm2 save
