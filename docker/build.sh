2ede451ce6f1c75b678ca841d20de54d4708927d
Soichi Hayashi <hayashis@iu.edu>
2018-03-02 18:55:58 +0000

    updated container version

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,9 @@
-docker build -t soichih/sca-event ..
+tag=1.0.2
+docker build -t soichih/event ..
 if [ ! $? -eq 0 ]; then
     echo "failed to build"
     exit
 fi
-docker tag soichih/sca-event soichih/sca-event:1.0.1
-docker push soichih/sca-event #latest
-docker push soichih/sca-event:1.0.1
+docker tag soichih/event soichih/event:$tag
+#docker push soichih/event #latest
+docker push soichih/event:$tag
