f5be3c522cb74e793090947d2f65868f13bb512e
Soichi Hayashi <hayashis@iu.edu>
2018-03-20 19:48:28 +0000

    Put apidoc under docs

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,8 @@
-tag=1.0.2
+tag=1.1.0
 docker build -t soichih/event ..
 if [ ! $? -eq 0 ]; then
     echo "failed to build"
     exit
 fi
 docker tag soichih/event soichih/event:$tag
-#docker push soichih/event #latest
 docker push soichih/event:$tag
